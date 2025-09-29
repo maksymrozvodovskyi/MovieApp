eb9640a4dce0280bbcfa0260cccf054bd39c3ecf
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-09-29 23:31:49 +0300

    fix

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -2,14 +2,14 @@ import { MovieCard } from "../MovieCard/MovieCard";
 import styles from "./MovieList.module.css";
 import type { MovieListProps } from "../../types/components";
 
-export const MovieList = ({ movies, genresMap }: MovieListProps) => {
+export const MovieList = ({ movies }: MovieListProps) => {
   if (!movies.length)
     return <p className={styles.noMovies}>No movies found.</p>;
 
   return (
     <div className={styles.list}>
       {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} genresMap={genresMap} />
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
   );
